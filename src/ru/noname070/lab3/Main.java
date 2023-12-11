b3e74b9477821293d4d8e8ec83a125217bbca511
NN <61068649+noname070@users.noreply.github.com>
2023-12-11 12:06:52 +0300

    minor commit

--- src/ru/noname070/lab3/Main.java
+++ src/ru/noname070/lab3/Main.java
@@ -2,7 +2,6 @@ package ru.noname070.lab3;
 
 public class Main {
     public static void main(String[] args)  {
-        Story story = new Story();
-        story.play();
+        Story.play();
     }
 }
